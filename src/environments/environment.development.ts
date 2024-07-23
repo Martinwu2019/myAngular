ac442d2d6cfd4ee12d54b15085a4ba635cb372d7
wutingsheng <martinwu2019@163.com>
2024-07-23 23:27:02 +0700

    envi

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://20.188.117.53:8080/api' // api server trong mode dev ở local
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://20.188.117.53:8080/api' //api server trong production
+};
+
