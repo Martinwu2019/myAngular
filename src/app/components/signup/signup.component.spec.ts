23e7a2a33def9d6ec6e4337df5a97cbe588a3d38
wutingsheng <martinwu2019@163.com>
2024-07-25 03:39:07 +0700

    test

--- src/app/components/signup/signup.component.spec.ts
+++ src/app/components/signup/signup.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { SignupComponent } from './signup.component';
 
@@ -8,6 +10,7 @@ describe('SignupComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [SignupComponent]
     })
     .compileComponents();
